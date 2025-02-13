c8e4c3c4767c87cf56c56c9e4c9567e0798445c8
Jonatan Männchen <jonatan@maennchen.ch>
2025-02-13 22:01:29 +0000

    brick files

--- lib/elixir/lib/access.ex
+++ lib/elixir/lib/access.ex
@@ -1,7 +1,3 @@
-# SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: 2021 The Elixir Team
-# SPDX-FileCopyrightText: 2012 Plataformatec
-
 defmodule Access do
   @moduledoc """
   Key-based access to data structures.
